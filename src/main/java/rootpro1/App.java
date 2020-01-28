1716f0b721dba756179d484ade057db0df17de74
root <root@localhost.localdomain>
2020-01-28 01:44:57 -0800

    1st

---
+++ src/main/java/rootpro1/App.java
@@ -0,0 +1,11 @@
+package rootpro1;
+    public class App {
+        public static void main(String[] args) {
+            String style = new String("Bold");
+            String style2 = new String("Bold");
+            if(style == style2)
+                System.out.println("Equal");
+            else
+                System.out.println("Not Equal");
+        }
+    }
